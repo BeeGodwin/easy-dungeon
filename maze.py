1cec8319fdaa32fd34a6c382068924c9f8c451b9
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2017-12-07 13:52:35 +0000

    Got as far as previously, with half as much code- nice.

--- maze.py
+++ maze.py
@@ -3,6 +3,7 @@ import random
 from copy import copy, deepcopy
 from tile import *
 
+
 class Maze:
     """the game board. Essentially a 2D array of tiles."""
 
@@ -19,8 +20,8 @@ class Maze:
         maze = [wall_top]
 
         for y in range(len(bool_mz)):
-
             row = [Wall(px=self.tile_px)]
+
             for x in range(len(bool_mz[0])):
                 if bool_mz[y][x]:
                     row.append(Tile(px=self.tile_px))
@@ -29,6 +30,7 @@ class Maze:
             row.append(Wall(px=self.tile_px))
 
             maze.append(row)
+
         maze.append(wall_bottom)
 
         return maze
@@ -44,13 +46,15 @@ def make_maze(size):
     """returns a maze ready for play."""
     mz = []
     r = int(sqrt(size))  # rooms per row
-    for i in range(size):
+
+    for i in range(size):  # make rows
         if i % 2 == 0:
             mz.append(make_row(size, r, False))
         else:
-            mz.append(make_row(size, r, True))
+            mz.append(join_rooms(mz[i - 1], make_row(size, r, True)))
 
-    # mz = complete_maze(mz)
+    # mz[-2] = join_rooms(mz[-1], mz[-2])  # has to be a better solution!
+
     return mz
 
 
@@ -71,41 +75,61 @@ def make_row(size, r, wall):
     return row
 
 
-# TODO - turn this method into Instantiate Tiles. Then we should be able to get back to playing it.
+def join_rooms(row_n, wall_row):
+    """Given row_n and a wall_row, modify wall_row."""
+    # for each room in row_n, flip a random bool in wall_row within its range.
+    rooms = []
+    current_room = []
+    for i in range(len(row_n)):
+        if row_n[i]:
+            current_room.append(i)
+        else:
+            rooms.append(copy(current_room))
+            current_room = []
+    if len(current_room) > 0:
+        rooms.append(current_room)
+
+    for room in rooms:
+        if len(room) > 1:
+            i = random.randrange(room[0], room[-1], 2)
+            wall_row[i] = True
+        else:
+            wall_row[room[0]] = True
+    return wall_row
 
 
+def get_adj_tiles(ary, x, y):  # func
+    """returns a list of (x, y) tuples describing adjacent coordinates in
+    array ary. Respects the edge of the board and corners."""
+    y_size = len(ary) - 1
+    x_size = len(ary[0]) - 1
+    lst = []
+    if y > 0:
+        lst.append((x, y - 1))
+    if x > 0:
+        lst.append((x - 1, y))
+    if x < x_size:
+        lst.append((x + 1, y))
+    if y < y_size:
+        lst.append((x, y + 1))
+    return lst
 
-#
-#
 
-#
-#
-# def join_rooms(row_n, wall_row):  # make func.
-#     """Given row_n and a wall_row, modify wall_row."""
-#     # for each room in row_n, flip a random bool in wall_row within its range.
-#     wrk = [copy(row_n)]
-#     for i in range(len(wrk[0])):
-#         rm_sz = flood_fill(wrk, i, 0)
-#         if rm_sz == 1:
-#             wall_row[i] = True
-#         elif rm_sz == 0:
-#             continue
-#         else:
-#             idx = random.randrange(i, rm_sz + i, step=2)
-#             wall_row[idx] = True
-#     return wall_row
-#
-#
-# def init_maze(size):  # is this the way to start? Could we start with row rooms?
-#     """returns a square 2d array of booleans, len size -2.
-#     Values are initialised with True for a path, False for a wall."""
-#     size -= 2
-#     paths = [True if i % 2 == 0 else False for i in range(size)]
-#     walls = [False for _ in range(size)]
-#     maze = [copy(paths) if i % 2 == 0 else copy(walls) for i in range(size)]
-#     return maze
-#
-#
+def list_room_coordinates(boolean_maze):
+    """returns a coordinate list of all the tiles in the room containing
+    tile at (x, y)."""
+    # TODO modify this function so that it returns a list of lists.
+    coord_lst = [(x, y)]
+
+    def recurse(x, y):
+        for (adj_x, adj_y) in get_adj_tiles(boolean_maze, x, y):
+            if (adj_x, adj_y) not in coord_lst and boolean_maze[adj_y][adj_x]:
+                coord_lst.append((adj_x, adj_y))
+                recurse(adj_x, adj_y)
+
+    recurse(x, y)
+    return coord_lst
+
 # def room_count(bools):  # is there a way to not have to copy before sending to this? also, func.
 #     """Counts rooms. Takes a 2d array of bools. Copy the array before
 #     sending (bad coding there!)"""
@@ -133,28 +157,13 @@ def make_row(size, r, wall):
 #     else:
 #         return n
 #
-# def get_adj_tiles(ary, x, y):  # func
-#     """returns a list of (x, y) tuples describing adjacent coordinates in
-#     array ary. Respects the edge of the board and corners."""
-#     y_size = len(ary) - 1
-#     x_size = len(ary[0]) - 1
-#     lst = []
-#     if y > 0:
-#         lst.append((x, y - 1))
-#     if x > 0:
-#         lst.append((x - 1, y))
-#     if x < x_size:
-#         lst.append((x + 1, y))
-#     if y < y_size:
-#         lst.append((x, y + 1))
-#     return lst
+
 #
 #
 # # def complete_maze(self, bools):  # see if you still need to do this when at bottom of rabbit hole
 # #     """Takes an almost-finished maze of boolean values. Ensures that the
 # #     maze consists of one 'room', and if not, modifies it so it does.
 # #     Returns completed maze of boolean values."""
-# #     # # TODO- solve the issue of orphaned rooms. still borked
 # #     # print(self.room_count(deepcopy(bools)))
 # #     # # plan B!
 # #     # while self.room_count(deepcopy(bools)) > 1:
@@ -163,17 +172,5 @@ def make_row(size, r, wall):
 # #
 # #     return bools
 #
-# def room_coords(bools, x, y):  # func again, given that it's used in construction.
-#     """returns a coordinate list of all the tiles in the room containing
-#     tile at (x, y)."""
-#     coord_lst = [(x, y)]
+
 #
-#     def recurse(x, y):
-#         for (adj_x, adj_y) in get_adj_tiles(bools, x, y):
-#             if (adj_x, adj_y) not in coord_lst and bools[adj_y][adj_x]:
-#                 coord_lst.append((adj_x, adj_y))
-#                 recurse(adj_x, adj_y)
-#
-#     recurse(x, y)
-#
-#     return coord_lst
--- test_dungeon.py
+++ test_dungeon.py
@@ -1,16 +1,11 @@
-from maze import Maze
 import maze
-from play_dungeon import *
-
-
+from maze import Maze
+import play_dungeon
 from player import Player
 from tile import Tile
 from tile import Wall
 import random
 from copy import deepcopy
-
-
-# TODO refactor tests such that they reflect the updated maze module.
 
 
 def test_make_rooms():
@@ -47,6 +42,26 @@ def test_move_is_legal():
     player.next_y = 2
     assert not mz.move_is_legal(player)
 
+
+def test_join_rooms():
+    random.seed(1)
+    walls = maze.join_rooms([True, True, True], [False, False, False])
+    assert walls == [True, False, False]
+
+
+def test_get_adj_tiles():
+    mz = [[True for _ in range(3)] for _ in range(3)]
+    assert maze.get_adj_tiles(mz, 1, 1) == [(1, 0), (0, 1), (2, 1), (1, 2)]
+    assert maze.get_adj_tiles(mz, 0, 0) == [(1, 0), (0, 1)]
+    assert maze.get_adj_tiles(mz, 2, 2) == [(2, 1), (1, 2)]
+
+
+def test_list_room_coordinates():
+    mz = [[True, False, True], [True, False, True], [True, False, True]]
+    assert maze.list_room_coordinates(mz) == [[(0, 0), (0, 1), (0, 2)],
+                                              [(2, 0), (2, 1), (2, 2)]]
+
+
 # def test_room_count():
 #     mz = Maze(size=3)
 #     assert mz.room_count([[False, False, False],
@@ -63,12 +78,7 @@ def test_move_is_legal():
 #                           [True, True, True]]) == 1
 #
 #
-# def test_get_adj_tiles():
-#     mz = Maze(size=3)
-#     maze = [[True for _ in range(3)] for _ in range(3)]
-#     assert mz.get_adj_tiles(maze, 1, 1) == [(1, 0), (0, 1), (2, 1), (1, 2)]
-#     assert mz.get_adj_tiles(maze, 0, 0) == [(1, 0), (0, 1)]
-#     assert mz.get_adj_tiles(maze, 2, 2) == [(2, 1), (1, 2)]
+
 #
 #
 # def test_flood_fill():
@@ -80,24 +90,7 @@ def test_move_is_legal():
 #     assert mz.flood_fill(bools, 0, 0) == 3
 #     assert bools == [[False, False, False], [False, False, False], [True, True, True]]
 #
-#
-# def test_make_rooms_in_row():
-#     mz = Maze(size=5)
-#     assert mz.make_rooms_in_row([True, False, True], 1) == [True, True, True]
-#     random.seed(1)  # random.randrange(2) returns 0
-#     assert mz.make_rooms_in_row([True, False, True, False, True], 2) == [True, True, True, False, True]
-#
-#
-# def test_join_rooms():
-#     mz = Maze(size=5)
-#     walls = mz.join_rooms([True, True, True], [False, False, False])
-#     random.seed(1)
-#     assert walls == [False, False, True]
-#
-#
 
-#
-#
 # # def test_complete_maze():  # bit borked!
 # #     mz = Maze(size=5)
 # #     bools = [[True, True, True], [False, False, True], [True, True, True]]
